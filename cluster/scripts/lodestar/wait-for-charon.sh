d6d5ebe512a03357d7ff9c4f16ee401f20d73f07
Hydepwns <drew@axol.io>
2026-01-26 22:14:32 +0100

    Fix: wait-for-charon.sh to check /livez instead of /readyz

--- cluster/scripts/lodestar/wait-for-charon.sh
+++ cluster/scripts/lodestar/wait-for-charon.sh
@@ -4,7 +4,7 @@ INFO="[ INFO | lodestar-wait ]"
 
 # Wait for Charon to be ready
 while true; do
-    status_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3620/readyz)
+    status_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3620/livez)
     if [ "$status_code" -eq 200 ]; then
         echo "$INFO Charon is ready. Lodestar can start."
         break
